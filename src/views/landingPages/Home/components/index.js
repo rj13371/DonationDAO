fe40ab72f34ff011649eee336d90e906cca27a64
rj1337 <mathesonroland@gmail.com>
2022-02-05 14:07:45 +0900

    rm demo

--- src/views/landingPages/Home/components/index.js
+++ src/views/landingPages/Home/components/index.js
@@ -1,4 +1,3 @@
-export { default as DemoPages } from './DemoPages';
 export { default as Features } from './Features';
 export { default as Footer } from './Footer';
 export { default as Hero } from './Hero';
