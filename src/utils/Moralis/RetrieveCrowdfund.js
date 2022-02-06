857dced61e4da723875fceb2a8f6dd8d529280fd
Roland M <rj1337@gmail.com>
2022-02-06 19:14:37 +0900

    add comments

--- src/utils/Moralis/RetrieveCrowdfund.js
+++ src/utils/Moralis/RetrieveCrowdfund.js
@@ -4,17 +4,6 @@ const RetrieveCrowdfund = async (id) => {
   const Crowdfund = Moralis.Object.extend('Crowdfund');
   const query = new Moralis.Query(Crowdfund);
   const result = await query.get(id);
-
-  //   const findCrowdfund = new Crowdfund();
-  //   findCrowdfund.id = id;
-
-  //   console.log(findCrowdfund);
-  //   const query = new Moralis.Query(Crowdfund);
-  //   query.equalTo('crowdfund', findCrowdfund);
-  //   const crowdfund = await query.find();
-
-  //   console.log(crowdfund);
-
   return result;
 };
 
